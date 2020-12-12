61ede15cc597537af10c92e31a5093d1ed0e546b
sunDay-code <changkaiyuan0916@gmail.com>
2020-12-11 21:29:42 -0800

    UPDATE

--- 007. Reverse Integer/007. Reverse Integer.py
+++ 007. Reverse Integer/007. Reverse Integer.py
@@ -1,1 +1,13 @@
-#Auto Generate
+class Solution:
+    def reverse(self, x: int) -> int:
+        isNeg=False
+        if x < 0:
+            x=-x
+            isNeg=True
+            
+        ans=int(str(x)[::-1])
+        if ans == 0 or ans >2**31-1 or ans <-2**31:
+            return 0
+        
+        return ans if not isNeg else -ans
+            
