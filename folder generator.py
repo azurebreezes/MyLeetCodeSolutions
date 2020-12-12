792f23a962bffba0cdb2d6c7de84c0f99ef872df
sunDay-code <changkaiyuan0916@gmail.com>
2020-12-11 20:42:30 -0800

    UPDATE

---
+++ folder generator.py
@@ -0,0 +1,17 @@
+#Create a folder with a txt file and a py file
+
+import os
+folder="007. Reverse Integer"
+if not os.path.exists(folder):
+    os.makedirs(folder)
+    write_path=f'{folder}/{folder}.txt'
+    f=open(write_path, 'w')
+    f.write("Auto Generate")
+    f.close()
+    write_path=f'{folder}/{folder}.py'
+    f=open(write_path, 'w')
+    f.write("#Auto Generate")
+    f.close()
+    print("Folder Created")
+else:
+    print("Folder exist")
