81a46d5c059a4a341e10c8d0ab1871747ca1675b
sunDay-code <changkaiyuan0916@gmail.com>
2021-01-11 22:17:20 -0800

    UPDATE

--- folder generator.py
+++ folder generator.py
@@ -1,7 +1,7 @@
 #Create a folder with a txt file and a py file in it
 
 import os
-folder="1662. Check If Two String Arrays are Equivalent"
+folder="088. Merge Sorted Array"
 if not os.path.exists(folder):
     os.makedirs(folder)
     write_path=f'{folder}/{folder}.txt'
