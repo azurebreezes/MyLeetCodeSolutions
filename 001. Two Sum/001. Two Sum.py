06e8e081c82596ccca9faa1ea051e723bb405346
sunDay-code <changkaiyuan0916@gmail.com>
2021-01-16 10:28:06 -0800

    UPDATE

--- 001. Two Sum/001. Two Sum.py
+++ 001. Two Sum/001. Two Sum.py
@@ -1,13 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}
         
-        dic={}
-        for i, num in enumerate(nums):
-            n = target - num
-            if num in dic:
-                return [dic[num], i]
-            else:
-                dic[n]=i
-        
-        return []
-            
+        for i in range(len(nums)):
+            if target - nums[i] in mp:
+                return [i, mp[target - nums[i]]]
+            else:  
+                mp[nums[i]] = i
--- folder generator.py
+++ folder generator.py
@@ -1,7 +1,7 @@
 #Create a folder with a txt file and a py file in it
 
 import os
-folder="215. Kth Largest Element in an Array"
+folder="001. Two Sum"
 if not os.path.exists(folder):
     os.makedirs(folder)
     write_path=f'{folder}/{folder}.txt'
