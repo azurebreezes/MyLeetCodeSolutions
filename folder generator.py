72e85fcca1ff250b1fea69fbe49daf91ac4ee6e4
sunDay-code <changkaiyuan0916@gmail.com>
2021-01-27 18:15:19 -0800

    UPDATE

--- folder generator.py
+++ folder generator.py
@@ -1,7 +1,7 @@
 #Create a folder with a txt file and a py file in it
 
 import os
-folder="953. Verifying an Alien Dictionary"
+folder="1680. Concatenation of Consecutive Binary Numbers"
 if not os.path.exists(folder):
     os.makedirs(folder)
     write_path=f'{folder}/{folder}.txt'
