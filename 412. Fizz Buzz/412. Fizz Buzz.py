c3054f3dd285e9a808ca2cccc724c9ed27a5666e
sunDay-code <changkaiyuan0916@gmail.com>
2021-01-29 09:10:35 -0800

    UPDATE

---
+++ 412. Fizz Buzz/412. Fizz Buzz.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                yield "FizzBuzz"
+            elif i % 5 == 0:
+                yield "Buzz"
+            elif i % 3 == 0:
+                yield "Fizz"
+            else:
+                yield str(i)
--- folder generator.py
+++ folder generator.py
@@ -1,7 +1,7 @@
 #Create a folder with a txt file and a py file in it
 
 import os
-folder="1663. Smallest String With A Given Numeric Value"
+folder="412. Fizz Buzz"
 if not os.path.exists(folder):
     os.makedirs(folder)
     write_path=f'{folder}/{folder}.txt'
