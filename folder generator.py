254447b032188517fb0e15532f4a2cbedb206c24
sunDay-code <changkaiyuan0916@gmail.com>
2021-02-08 23:12:08 -0800

    update

--- folder generator.py
+++ folder generator.py
@@ -1,7 +1,7 @@
 #Create a folder with a txt file and a py file in it
 
 import os
-folder="821. Shortest Distance to a Character"
+folder=""
 if not os.path.exists(folder):
     os.makedirs(folder)
     write_path=f'{folder}/{folder}.txt'
