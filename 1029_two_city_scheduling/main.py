b7efa11e262d02dd3df44991a259af368ce35e13
Azurebreezes <changkaiyuan0916@gmail.com>
2022-03-26 09:36:08 -0700

    Modified comments in 1029

--- 1029_two_city_scheduling/main.py
+++ 1029_two_city_scheduling/main.py
@@ -2,12 +2,12 @@ class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         n = len(costs)//2
         # Sort the costs list base on the difference between aCost and bCost
-        # Then costs[0] will have the minimum aCost, 
-        # cost[n-1] will have the minimum bCost.
         costs.sort(key = lambda x : x[0] - x[1])            
 
         ans = 0
-        # Each iteration, add both minimum aCost and bCost
+        # From i in range 0 to n, the costs[i] will have a lower aCost. 
+        # For range n + i to 2n, the costs[n+i] will have a lower bCost.
+        # For each iteration, add the lower aCost and bCost from the list
         for i in range(n):
             ans += costs[i][0] + costs[n+i][1]
         return ans
